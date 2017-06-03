47ae0a1954d40b56d0cd18d8e94ba2e42e0587c9
Clint Goodman <clintgoodman@workfront.com>
2017-06-03 16:04:54 -0600

    feat: adding base case to start from

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 import { fetchCharacters } from './lib'
 
-const base = {
-  version: 1,
-  purpose: 'EDUCATION',
-}
-
 export const fetchAndTransform = () =>
   fetchCharacters()
-    .then(({ results }) =>
-      results.map(({ name, homeworld: home, url, films: [firstMovie, ...otherMovies] }) =>
-        ({
-          ...base,
-          name,
-          home,
-          url,
-          firstMovie,
-          otherMovies: otherMovies.length,
-        })
-      )
-    )
+    .then(() => {
+      return [{}]
+    })
